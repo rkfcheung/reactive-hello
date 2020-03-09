20a169f4474d31a7457ede3d423c46252f06e0f5
Raymond Cheung <korekontakt@gmail.com>
2020-03-09 22:02:33 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.70"
 	kotlin("plugin.spring") version "1.3.70"
-	maven
 }
 
 group = "com.rkfcheung.reactive"
